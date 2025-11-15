a6746e615f29af524c5d5d5b4b3a703bcd243f89
Milson Munakami <milsonmun@yahoo.com>
2025-11-14 19:36:19 -0500

    Testing DockerHub Registry

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From NERC-OCP - Test Me Quay.io!" }');
+    res.send('{ "response": "Hello From NERC-OCP - Test Me DockerHub!" }');
 });
 
 app.get('/will', function (req, res) {
