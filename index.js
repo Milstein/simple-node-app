fbd2238165e705587cd21e5338f7b609687c7afe
Milstein <milsonmun@yahoo.com>
2025-12-05 11:34:55 -0500

    final test using GHCR

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From NERC-OCP Testing!" }');
+    res.send('{ "response": "Hello From NERC-OCP!" }');
 });
 
 app.get('/will', function (req, res) {
